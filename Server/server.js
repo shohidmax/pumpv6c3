6e54dcef899710555251821d4101d34f654ba888
shohidmax <shohidmax@gmail.com>
2026-04-02 12:42:56 +0600

    hello16

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 
 // --- MongoDB Setup ---
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/motor_data";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/motor_data6c";
 mongoose.connect(MONGODB_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error('MongoDB connection error:', err));
